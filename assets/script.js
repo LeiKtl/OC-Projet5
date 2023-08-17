913917e4d6b6b2e6716c6ed9ca173fe751a1d465
LeiKtl <leilaktl.pro@gmail.com>
2023-08-17 10:56:13 +0200

    Ajout variables et dots dans JS

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,9 @@
+// Les variables //
 const arrowLeft = document.querySelector(".arrow_left");
 const arrowRight = document.querySelector(".arrow_right");
-
-arrowLeft.addEventListener("click", function() {
-	console.log("gauche");
-})
-	
-
-
-arrowRight.addEventListener("click", function() {
-	console.log("droite");
-})
-	
-
-
+const image = document.querySelector(".banner-img");
+const text = document.querySelector("#banner").getElementsByTagName("p");
+const dots = document.querySelector(".dots");
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -31,3 +22,38 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+let slidePosition = 0 ;
+let numbSlide = slides.length;
+
+// Creation des dots
+for (i=0 ; i < numbSlide ; i++) {
+	let dot = document.createElement("div");
+	dot.classList.add("dot");
+	dots.appendChild(dot);
+
+	if (i === slidePosition){
+		dot.classList.add("dot_selected");
+	} else {
+		dot.classList.remove("dot_selected");
+	}
+}
+
+//Click gauche
+arrowLeft.addEventListener("click", function() {
+	//if (slides.images <= slides.lenght || slides.images > slides.lenght) {
+	//	slides[0] ++1;
+	//}
+
+	//if image/texte est <= à .lenght du tableau ou > à 0
+	// image et texte -1
+	// dot = .dot_selected
+	console.log("gauche");
+})
+	
+//Click droit
+arrowRight.addEventListener("click", function() {
+	//if image/texte est >= 0 et < .length
+	// image et texte +1
+	// dot = .dot_selected
+	console.log("droite");
+})
