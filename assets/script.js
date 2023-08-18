ab66a99a8ab0760fb1ce30c0bc9c5cc0f087a8c6
LeiKtl <leilaktl.pro@gmail.com>
2023-08-18 10:29:37 +0200

    Ajout changemt image+tagLine. Réorganisation code

--- assets/script.js
+++ assets/script.js
@@ -2,58 +2,60 @@
 const arrowLeft = document.querySelector(".arrow_left");
 const arrowRight = document.querySelector(".arrow_right");
 const image = document.querySelector(".banner-img");
-const text = document.querySelector("#banner").getElementsByTagName("p");
+const text = document.querySelector(".banner-text");
 const dots = document.querySelector(".dots");
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"./assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"./assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"./assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"./assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 let slidePosition = 0 ;
-let numbSlide = slides.length;
-
-// Creation des dots
-for (i=0 ; i < numbSlide ; i++) {
-	let dot = document.createElement("div");
-	dot.classList.add("dot");
-	dots.appendChild(dot);
-
-	if (i === slidePosition){
-		dot.classList.add("dot_selected");
-	} else {
-		dot.classList.remove("dot_selected");
-	}
-}
+const numbSlide = slides.length;
 
 //Click gauche
 arrowLeft.addEventListener("click", function() {
-	//if (slides.images <= slides.lenght || slides.images > slides.lenght) {
-	//	slides[0] ++1;
-	//}
-
-	//if image/texte est <= à .lenght du tableau ou > à 0
-	// image et texte -1
-	// dot = .dot_selected
+	slidePosition --;
+	image.src = slides[slidePosition].image;
+	text.innerHTML = slides[slidePosition].tagLine;
 	console.log("gauche");
 })
 	
 //Click droit
 arrowRight.addEventListener("click", function() {
-	//if image/texte est >= 0 et < .length
-	// image et texte +1
-	// dot = .dot_selected
+	slidePosition ++;
+	image.src = slides[slidePosition].image;
+	text.innerHTML = slides[slidePosition].tagLine;
 	console.log("droite");
 })
+
+// Creation des dots
+for (i=0 ; i < numbSlide ; i++) {
+	let dot = document.createElement("div");
+	dot.classList.add("dot");
+	dots.appendChild(dot);	
+	if (i === slidePosition) {
+		dot.classList.add("dot_selected");
+	}
+}
+
+//function dotChange() {
+//	if (i === slidePosition) {
+//			dot.classList.add("dot_selected");
+//		} else {
+//			dot.classList.remove("dot_selected");
+//		}
+//		console.log("ca marche!");
+//}
