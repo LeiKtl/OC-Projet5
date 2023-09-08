b3ce3b7cbcadfd6c5836ac747137ec7134116b83
LeiKtl <leilaktl.pro@gmail.com>
2023-09-08 10:55:56 +0200

    Modifications après soutenance

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,24 @@
 let slidePosition = 0 ;
+const slides = [
+	{
+		"image":"slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	
+	},
+	{
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		
+	},
+	{
+		"image":"slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
 
 function slider() {
 	// Les variables //
@@ -18,25 +38,6 @@ function slider() {
 	arrowRight.alt = "flèche pour cliquer à droite";
 	banner.appendChild(arrowRight);
 
-	const slides = [
-		{
-			"image":"slide1.jpg",
-			"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-		},
-		{
-			"image":"slide2.jpg",
-			"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-		},
-		{
-			"image":"slide3.jpg",
-			"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-		},
-		{
-			"image":"slide4.png",
-			"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-		}
-	]
-
 	const numbSlide = slides.length;
 
 	//Click gauche
@@ -45,7 +46,7 @@ function slider() {
 		if (slidePosition < 0) {
 			slidePosition = numbSlide -1;
 		}
-		slideChanges(slides);
+		slideChanges();
 	})
 		
 	//Click droit
@@ -54,7 +55,7 @@ function slider() {
 		if (slidePosition === numbSlide) {
 			slidePosition = 0;
 		}
-		slideChanges(slides);
+		slideChanges();
 	})
 
 	// Création des dots
@@ -82,12 +83,14 @@ function selectedDot() {
 }
 
 // Fonction pour selection image, texte et dot selon position slide
-function slideChanges(slides) {
+function slideChanges() {
 	const image = document.querySelector(".banner-img");
 	const text = document.querySelector("#banner p");
 	image.src = `./assets/images/slideshow/${slides[slidePosition].image}`;
 	text.innerHTML = slides[slidePosition].tagLine;
 	selectedDot();
 }
+// Appel de la fonction pour actualiser le slider
+slideChanges();
 
 slider();
